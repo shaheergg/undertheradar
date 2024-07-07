e4c623d030dc9da0b31531baa007a104f88d22e9
shaheersystems <shaheer.imran1429@gmail.com>
2024-07-07 23:15:13 +0500

    checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Checkout from "./pages/Checkout";
+import Purchased from "./pages/Purchased";
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/purchased" element={<Purchased />} />
       </Routes>
     </>
   );
